a6d5a6138290548d2e4b1c6736037139489518c4
Vincent REHM <contact@vincentrehm.fr>
2021-06-08 18:14:21 +0200

    strapi create app and modeling

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e7233786d56e9860e2e0f708f41d798'),
+    },
+  },
+});
